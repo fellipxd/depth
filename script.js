ec3fd46da1b4617e3880c7e09c6f4c6f0b41c8b2
Philip <philipokedi@gmail.com>
2021-07-12 20:24:31 +0100

    feat: brand and footer setions plus styling

--- script.js
+++ script.js
@@ -37,5 +37,56 @@ formClose.addEventListener('click', () => {
     loginForm.classList.remove('active');
   });
 
+  var swiper = new Swiper(".review-slider", {
+      spaceBetween:20,
+      loop: true,
+      autoplay:{
+          delay:2500,
+          disableOnInteraction: false,
+      },
+      breakpoints:{
+          640:{
+              slidesPerView:1,
+          },
+          768:{
+            slidesPerView:2,
+        },
+        1024:{
+            slidesPerView:3,
+        },
+    
+      },
+     
+  });
+
+
+  var swiper = new Swiper(".brand-slider", {
+    spaceBetween:20,
+    loop: true,
+    autoplay:{
+        delay:2500,
+        disableOnInteraction: false,
+    },
+    breakpoints:{
+        450:{
+            slidesPerView:2,
+        },
+        768:{
+            slidesPerView:3,
+        },
+        991:{
+            slidesPerView:4,
+        },
+        1200:{
+            slidesPerView:5,
+        },}
+       
+       
+       
+       
+   
+});
+
+
 
 
